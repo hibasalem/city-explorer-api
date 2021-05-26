2be4b763317862d5cdcd02c8a4bc9023c52ad6d8
hibasalem <salemhiba.hs@gmail.com>
2021-05-26 17:50:01 +0300

    finished backend

--- server.js
+++ server.js
@@ -12,60 +12,47 @@ server.get('/', (req, res) => {
     res.send('home route')
 })
 
-server.get('/test', (req, res) => {
-    let str = "test back end "
-    res.send(str);
-})
-
 server.get('/dataTest', (req, res) => {
-    res.send('weatherData')
+    res.send(weatherData)
 })
-
-var today = new Date();
-// a methot from stack over flow  to show currint date and time 
 
 class ForeCast {
-
-    constructor(date) {
-
-        date = today;
-        description = ` low temp ${weatherData.data.low_temp}, max_temp ${weatherData.data.max_temp} with ${weatherData.data.description} wind spd ${weatherData.data.wind_spd} clouds ${weatherData.data.clouds} `;
-
+    constructor(item) {
+        this.date = item.valid_date;
+        this.description = item.weather.description;
+        // this.description = ` low temp ${weatherData.data.low_temp}, max_temp ${weatherData.data.max_temp} with ${weatherData.data.description} wind spd ${weatherData.data.wind_spd} clouds ${weatherData.data.clouds} `;
     }
 }
 
 //http://localhost:3001/weather?city_name=Amman
 
 server.get('/weather', (req, res) => {
-
-    let cityitems = req.query.city_name;
+    let cityName = req.query.city_name;
     let cityData = weatherData.find(item => {
 
-        if (item.city_name == cityitems) {
-
-           let cityitem = new ForeCast(cityitems);
-
-            return [item.lat, item.lon, item.city_name , cityitem];
-
+        if (cityName.toLowerCase() == item.city_name.toLowerCase()) {
+            //    let cityItem = new ForeCast(cityItems);
+            // return [item.lat, item.lon, item.city_name , cityitem];
+            return item;
         };
+    })
 
+    try {
 
-    });
-
-    console.log(cityData);
-
-    if (cityData) {
-
-        res.send(cityData);
-
-    } else if (!cityData) {
+        let forecasts = cityData.data.map(item => {
+            return new ForeCast(item);
+        })
+        // return new Forecast(item);
+        res.send(forecasts);
+    } 
+    catch {
         res.status(500).send('we dont have this city');
     };
 
 });
 
 server.get('*', (req, res) => {
-    res.status(500).send('Something went wrong');
+    res.status(404).send('Something went wrong');
 });
 
 
